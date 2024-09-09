71d72649e604a69ba5f8c5fb6843b901809af38a
Joel Lacerda <joellacerdaol@gmail.com>
2024-09-09 18:02:04 -0300

    Initial Commit

---
+++ Moonshot/MoonshotApp.swift
@@ -0,0 +1,17 @@
+//
+//  MoonshotApp.swift
+//  Moonshot
+//
+//  Created by Joel Lacerda on 09/09/24.
+//
+
+import SwiftUI
+
+@main
+struct MoonshotApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
