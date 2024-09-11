13143eb372e861741a575ea376d422539242f042
Joel Lacerda <joellacerdaol@gmail.com>
2024-09-10 21:07:25 -0300

    add json files

---
+++ Moonshot/Astronaut.swift
@@ -0,0 +1,14 @@
+//
+//  Astronaut.swift
+//  Moonshot
+//
+//  Created by Joel Lacerda on 10/09/24.
+//
+
+import Foundation
+
+struct Astronaut: Codable, Identifiable {
+    let id: String
+    let name: String
+    let description: String
+}
